358b08b68d4bcd9f534001853d9c4249a66d5e21
Sebastian Mejia <Sebastian.mejia10@udea.edu.co>
2026-03-19 17:35:12 -0500

    Agrego archivos de la tarea

---
+++ pipeline.sh
@@ -0,0 +1,2 @@
+wget -O exoplanetas.csv "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+pl_name,pl_rade,pl_bmasse+FROM+ps+WHERE+pl_rade+IS+NOT+NULL+AND+pl_bmasse+IS+NOT+NULL&amp;format=csv"
+
