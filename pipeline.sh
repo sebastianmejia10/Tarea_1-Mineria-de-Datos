2506f4b5b2a3a5ecba15058e99f04cf2f418ea92
sebastianmejia10 <sebastian.mejia10@udea.edu.co>
2026-03-21 18:07:50 +0000

    Moidificaciones finales

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,3 @@
 wget -O exoplanetas.csv "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+pl_name,pl_rade,pl_bmasse+FROM+ps+WHERE+pl_rade+IS+NOT+NULL+AND+pl_bmasse+IS+NOT+NULL&amp;format=csv"
-python3 constructor_bd.py
+python3 constructor_db.py
 python3 analisis_visual.py
